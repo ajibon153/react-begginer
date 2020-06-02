eb0a350e17f4734d0cbe9dccbcad88a90c350684
Fauji <fauji@hotelmurah.com>
2020-06-02 22:11:34 +0700

    space and token

--- src/5BeachResort/Contenfull.js
+++ src/5BeachResort/Contenfull.js
@@ -1,9 +1,9 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  //   space: "koz886ahmg12",
-  //   accessToken: "KeAJYS4fJPQ74Cw43BuXRQdrn0SIurdW-bv_yH1GHm8",
+  space: "koz886ahmg12",
+  accessToken: "KeAJYS4fJPQ74Cw43BuXRQdrn0SIurdW-bv_yH1GHm8",
 
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_API_ACCESS_TOKEN,
+  //   space: process.env.REACT_APP_API_SPACE,
+  //   accessToken: process.env.REACT_APP_API_ACCESS_TOKEN,
 });
